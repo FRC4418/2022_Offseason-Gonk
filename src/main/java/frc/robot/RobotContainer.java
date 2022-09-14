31f339f33d83f1621948d05c6c41bd54f3cd462b
Auwunm <autumn.bernstein05@gmail.com>
2022-09-13 19:35:07 -0600

    Added default commands

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -16,6 +16,7 @@ import frc.robot.subsystems.Shooter;
 import frc.robot.subsystems.Intake;
 import frc.robot.commands.DrivetrainDrive;
 import frc.robot.commands.IntakeUp;
+import frc.robot.commands.ShooterEject;
 import frc.robot.commands.ShooterShoot;
 import frc.robot.commands.blankAuto;
 import frc.robot.commands.climberDown;
@@ -48,7 +49,8 @@ public class RobotContainer {
   public RobotContainer() {
     // Configure the button bindings
     configureButtonBindings();
-
+    shooter.setDefaultCommand(new ConveyorIdle(shooter, intake));
+    intake.setDefaultCommand(new IntakeUp(intake));
   }
 
   /**
@@ -66,6 +68,7 @@ public class RobotContainer {
     driver.getDPadDown().whenHeld(new climberDown(climber));
 
     driver.getRightButton().whenHeld(new ShooterShoot(shooter, intake));
+    driver.getLeftButton().whenHeld(new ShooterEject(shooter, intake));
   }
 
   /**
