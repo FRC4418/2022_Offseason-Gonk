bf3774a39132680f0de3146f53b96d1151b51554
FRCImpulse4418 <first4418@gmail.com>
2022-10-09 10:34:12 -0600

    Revert "added camera"
    This reverts commit 7b7e407cb6dd919f64e3b7cb77de08d1a8c0bed6.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,9 +7,6 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import edu.wpi.first.cameraserver.CameraServer;
-import edu.wpi.first.wpilibj.TimedRobot;
-
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -31,7 +28,6 @@ public class Robot extends TimedRobot {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
-    CameraServer.startAutomaticCapture(1);
   }
 
   /**
@@ -106,4 +102,3 @@ public class Robot extends TimedRobot {
   @Override
   public void simulationPeriodic() {}
 }
-
