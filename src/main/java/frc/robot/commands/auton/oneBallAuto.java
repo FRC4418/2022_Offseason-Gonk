9df17b7382836e79b384bea992a5e25ff2aa32f2
FRCImpulse4418 <first4418@gmail.com>
2022-11-01 18:49:18 -0600

    auto stuff

--- src/main/java/frc/robot/commands/auton/oneBallAuto.java
+++ src/main/java/frc/robot/commands/auton/oneBallAuto.java
@@ -4,22 +4,23 @@
 
 package frc.robot.commands.auton;
 
-
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.commands.shooterShoot;
 import frc.robot.subsystems.Drivetrain;
-import frc.robot.commands.auton.InfineteDrive;
-import frc.robot.constants.Settings;
+import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.Shooter;
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
-public class realAutoCode extends ParallelRaceGroup {
-  /** Creates a new realAutoCode. */
-  public realAutoCode(Drivetrain drivetrain) {
+public class oneBallAuto extends SequentialCommandGroup {
+  /** Creates a new oneBallAuto. */
+  public oneBallAuto(Shooter shooter, Intake intake, Drivetrain drivetrain) {
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
-    addCommands(new WaitCommand(Settings.Drivetrain.TIME_MOVING.get()), new InfineteDrive(drivetrain));
+    addCommands(new AutoShoot(shooter, intake), new WaitCommand(2.0));
+    addCommands(new realAutoCode(drivetrain));
+    addRequirements(shooter);
   }
-    }
+}
